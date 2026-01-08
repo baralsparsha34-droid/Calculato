a13f3585717cd4442a2cc905790d07730e62e73b
Sparsha Baral <baralsparsha34@gmail.com>
2026-01-08 13:31:09 +0545

    Calculate now

--- Calculato.py
+++ Calculato.py
@@ -1,77 +1,83 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,session
 import os
 app=Flask(__name__)
-current_expression="0"#For saving and adding logic and condition from expression displayed expression(Also for showing result combined with the before expressin and newer.)
-answer="0"#for (Ans)
+app.secret_key="Maths3021"
 @app.route("/",methods=["GET","POST"]) 
-#Main function                                                         
+#Main function   
 def mainfunc():
-    #Global vaiables can be used anywhere:
-    global expression
-    global current_expression
-    global funcer
-    global answer
-    #For instructions.
+    #Initializing the values by deafult.
+    #current_expression is for the newer value.
+    current_expression= session.get("current_expression","0")#0 as deafult
+    expression= session.get("expression","0")#0 as deafult
+    #expression is for the first and the starting value.
+    answer= session.get("answer","0") #0 as deafult
     if request.method=="POST":
         numbers=request.form.get("num_button")
         oper=request.form.get("op_button")
         func=request.form.get("func_button")
         expected_option= numbers or oper or func
-        expression=request.form.get("display_box") or current_expression
+        session["expression"]=request.form.get("display_box") or session["current_expression"] 
         #If any options/buttons are pressed.
         if expected_option:
+            print("POSTED")
             #Checking for number (options/buttons).
             if expected_option == numbers:
-                if expression=="0" and expected_option!=".":
-                    current_expression=expression.replace("0",expected_option)
-                elif expected_option=="." and expression[-1] in "1234567890" or expression[-1]=="3.14":#type:ignore
-                    current_expression= expression + expected_option
+                if session["expression"]=="0" and expected_option!=".":
+                    session["current_expression"]=session["expression"].replace("0",expected_option)
+                elif expected_option=="." and session["expression"][-1] in "1234567890" or session["expression"][-1]=="3.14":#type:ignore
+                    session["current_expression"]= session["expression"] + expected_option
                 else:
-                    current_expression=expression + expected_option
+                    session["current_expression"]=session["expression"] + expected_option
             #Checking for operator (options/buttons).
             elif expected_option == oper:
-                if expression [-1] in "+-*/":
-                    current_expression= expression+expected_option
-                elif expression[-1] in "1234567890":
-                    current_expression=expression+expected_option
+                if session["expression"] [-1] in "+-*/":
+                    session["current_expression"]= session["expression"]+expected_option
+                elif session["expression"][-1] in "1234567890":
+                    session["current_expression"]=session["expression"]+expected_option
             #Checking for functionalble (options/buttons).
             elif expected_option == func:
             #Checking for (AC/Clear all) (options/buttons).
                 if expected_option =="AC":
-                    current_expression="0"
+                    session["current_expression"]="0"
             #Checking for (Del/Delete) (options/buttons).
                 elif expected_option=="Del":
-                    if expression!="0" and  len(expression)>1:
-                        current_expression= expression[:-1]
+                    if session["expression"]!="0" and  len(session["expression"])>1:
+                        session["current_expression"]= session["expression"][:-1]
                     else:
-                        current_expression="0"  
+                        session["current_expression"]="0"  
             #Checking for (Ans/Anwer holder) (options/buttons).
                 elif expected_option=="Ans":
-                    if expression[-1] in "+-/*":
-                        current_expression=expression+ answer
-                    elif expression =="0":
-                        current_expression = answer
+                    if session["expression"][-1] in "+-/*":
+                        session["current_expression"]=session["expression"]+session["answer"]
+                    elif session["expression"] =="0":
+                        session["current_expression"] = session["answer"]
             #Checking for (Calc/Calculating)(options/buttons),Main function.
                 elif expected_option=="Calc":
                     try:#For handling aswell as displaying errors!
-                        funcer="="
-                        the_result=str(eval(expression))                   
-                        current_expression=the_result#Giving output in diplay_box
-                        answer=the_result#Saving the result.
+                        the_result=str(eval(session["expression"]))                   
+                        session["current_expression"]=the_result#Giving output in diplay_box
+                        session["answer"]=the_result#Saving the result.
                     except ValueError as vale:
                         print("Value Eror",vale)
-                        current_expression="#Value Error!"
+                        session["current_expression"]="#Value Error!"
                     except ZeroDivisionError as zero:
                         print("Zero Division Error!",zero)
-                        current_expression="#Zero Division Error!"
+                        session["current_expression"]="#Zero Division Error!"
                     except SyntaxError as sin:
                         print("Invalid Syntax!",sin)
-                        current_expression="#Syntax Error!"
+                        session["current_expression"]="#Syntax Error!"
                     except Exception as el:
                         print("Unknown Error Ocuured!",el)
-                        current_expression="#Unknown Error Ocuured!"
+                        session["current_expression"]="#Unknown Error Ocuured!"
+            #Modifying the session and updating the values.
+            # session["current_expression"]=current_expression the session gets changed.
+            current_expression=session["current_expression"]#the varibale gets changed.
+            session["expression"]=current_expression#updating older value by newer value
+            answer =session.get("answer","0")   
+            session.modified=True                                               
         return redirect("/")#For handling refresh and redirecting program to main.
-    return render_template("App.html",expression=current_expression,answer=answer)
+    print(expression)
+    return render_template("App.html",expression=expression,answer=answer)
 #Logic to run the app in the certain port and by he ceratin file only.(Makes it secure and private)
 if __name__=="__main__":
     ported=int(os.environ.get("PORT",3021))
